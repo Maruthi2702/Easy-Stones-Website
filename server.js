68e7f09f4dea394a4c0e17fe229cdce3d0cda4ce
Maruthi2702 <68773919+Maruthi2702@users.noreply.github.com>
2025-11-26 12:59:48 -0800

    Fix server.js: Update catch-all route for Express 5

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ if (fs.existsSync(distPath)) {
   
   // The "catchall" handler: for any request that doesn't
   // match one above, send back React's index.html file.
-  app.get('*', (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 }
