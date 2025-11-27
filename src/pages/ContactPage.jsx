f866c36045f774afba4c5d07971044e96f256a53
Maruthi2702 <68773919+Maruthi2702@users.noreply.github.com>
2025-11-26 18:35:04 -0800

    Fix Google Maps embed URL for contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -86,7 +86,7 @@ const ContactPage = () => {
                     {/* Embedded Map */}
                     <div className="map-container">
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2702.5!2d-122.23!3d47.38!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2s22601%2076th%20Ave%20S%2C%20Kent%2C%20WA%2098032!5e0!3m2!1sen!2sus!4v1234567890123!5m2!1sen!2sus"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2702.5!2d-122.2347!3d47.3803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54905d0b8c8c8c8d%3A0x1234567890abcdef!2s22601%2076th%20Ave%20S%2C%20Kent%2C%20WA%2098032!5e0!3m2!1sen!2sus!4v1732672800000!5m2!1sen!2sus"
                             width="100%"
                             height="300"
                             style={{ border: 0, borderRadius: '12px' }}
